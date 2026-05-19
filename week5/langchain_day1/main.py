37b52eb002678a579feabd24105d8ba54915bb4f
ShrutishreePanda <shrutipanda31@gmail.com>
2026-05-19 17:46:57 +0530

    Implemented prompt templates and dynamic prompts

--- week5/langchain_day1/main.py
+++ week5/langchain_day1/main.py
@@ -1,7 +1,18 @@
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
 
 llm = ChatOllama(model="llama3")
 
-response = llm.invoke("Explain LangChain simply")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a senior AI architect specializing in LLM systems, LangChain, Retrieval-Augmented Generation (RAG),"
+    "AI agents, and enterprise AI workflows."),
+    ("human", "Explain the AI concept: {topic}")
+])
+
+formatted_prompt = prompt.invoke({
+    "topic": "Retrieval-Augmented Generation (RAG)"
+})
+
+response = llm.invoke(formatted_prompt)
 
 print(response.content)
